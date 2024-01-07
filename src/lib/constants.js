c51badf9091e943ed0ed495dfcfb9e5e295c236a
Antonov-S <anto1nov1@gmail.com>
2024-01-07 21:24:24 +0200

    Adding a basic markup for Item and ItemList components

---
+++ src/lib/constants.js
@@ -0,0 +1,24 @@
+export const initialItems = [
+  {
+    id: 1,
+    name: "good mood",
+    packed: false
+  },
+  {
+    id: 2,
+    name: "passport",
+    packed: false
+  },
+  {
+    id: 3,
+    name: "phone charger",
+    packed: false
+  }
+];
+
+export const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items"
+];
