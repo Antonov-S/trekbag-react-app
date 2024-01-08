91f290b438185c9a093b05f1846aa65bf55af1a0
Antonov-S <anto1nov1@gmail.com>
2024-01-09 01:39:43 +0200

    Finishing Count component and make it dynamic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -63,7 +63,10 @@ function App() {
       <BackgroundHeading />
 
       <main>
-        <Header />
+        <Header
+          totalNumberOfItems={items.length}
+          numbersOfItemsPacked={items.filter(item => item.packed).length}
+        />
         <ItemList
           items={items}
           handleDeleteItem={handleDeleteItem}
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 
-export default function Counter() {
+export default function Counter({ totalNumberOfItems, numbersOfItemsPacked }) {
     return (
-        <p><b>1</b> / 3 items packed</p>
+        <p><b>{numbersOfItemsPacked}</b> / {totalNumberOfItems} items packed</p>
     )
 }
